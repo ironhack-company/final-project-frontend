dcbd718ec386e8215bd7098726652eb2805405eb
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-10 11:39:42 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,59 +1,88 @@
-import React, {Component, Fragment} from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-import Home from './components/home/Home';
-import NotFound from './components/404/NotFound.js';
-import SignUp from './components/auth/SignUp';
-import LogIn from './components/auth/LogIn';
-import Profile from './components/profile/Profile'
-import actions from './services/index'
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import Home from "./components/home/Home";
+import NotFound from "./components/404/NotFound.js";
+import SignUp from "./components/auth/SignUp";
+import LogIn from "./components/auth/LogIn";
+import Profile from "./components/profile/Profile";
+import actions from "./services/index";
+import moduleName from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import css from "./index.css";
 
 class App extends Component {
-  
-  state = { }
-  
+  state = {};
+
   async componentDidMount() {
-    let user = await actions.isLoggedIn()
-    this.setState({...user.data})
+    let user = await actions.isLoggedIn();
+    this.setState({ ...user.data });
   }
 
-  setUser = (user) => this.setState(user)
-  
+  setUser = user => this.setState(user);
+
   logOut = async () => {
-    let res = await actions.logOut()
-    this.setUser({email:null, createdAt: null, updatedAt: null, _id: null }) //FIX 
-  }
+    let res = await actions.logOut();
+    this.setUser({ email: null, createdAt: null, updatedAt: null, _id: null }); //FIX
+  };
 
-  render(){
-
+  render() {
     return (
-    <BrowserRouter>
-      {this.state.email}
-      <nav>
-        <NavLink to="/">Home |</NavLink>
-  
-        {this.state.email ? 
-          <Fragment>
-           <NavLink onClick={this.logOut} to='/'>Log Out |</NavLink> 
-           <NavLink to="/profile">Profile|</NavLink>
-           </Fragment>
-           :
-           <Fragment>
-           <NavLink to="/sign-up">Sign Up |</NavLink>
-           <NavLink to="/log-in">Log In |</NavLink>
-           </Fragment>
-          }
-        
-      </nav>
-      <Switch>
-        <Route exact path="/" render={(props) => <Home {...props} />} />
-        <Route exact path="/sign-up" render={(props)=><SignUp {...props} setUser={this.setUser} />} />
-        <Route exact path="/log-in" render={(props) => <LogIn {...props} setUser={this.setUser}/>} />
-        <Route exact path="/profile" render={(props) => <Profile {...props} user={this.state}/>} />
+      <BrowserRouter>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>
+            <NavLink to="/">TripApp</NavLink>
+          </Navbar.Brand>
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Input your destination"
+              className="mr-sm-2"
+            />
+            {/* <Button variant="outline-info">Search</Button> */}
+          </Form>
+          <Nav className="mr-auto">
+            <NavLink to="/">Home</NavLink>
+          </Nav>
+          <Nav>
+            {this.state.email ? (
+              <Fragment>
+                {this.state.email}
+                <NavLink to="/profile">Profile</NavLink>
+                <NavLink className="NavLink" onClick={this.logOut} to="/">
+                  Log Out
+                </NavLink>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <NavLink to="/sign-up">Sign Up</NavLink>
+                <NavLink to="/log-in">Log In</NavLink>
+              </Fragment>
+            )}
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" render={props => <Home {...props} />} />
+          <Route
+            exact
+            path="/sign-up"
+            render={props => <SignUp {...props} setUser={this.setUser} />}
+          />
+          <Route
+            exact
+            path="/log-in"
+            render={props => <LogIn {...props} setUser={this.setUser} />}
+          />
+          <Route
+            exact
+            path="/profile"
+            render={props => <Profile {...props} user={this.state} />}
+          />
 
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
-  );
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 export default App;
