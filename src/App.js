dfa31a498448b3c473fe85094fc3cd940563fafe
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-10 16:53:04 -0500

    add SignUp in Home

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ import Profile from "./components/profile/Profile";
 import actions from "./services/index";
 import moduleName from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Navbar,
-  Nav,
-  Form,
-  FormControl,
-  Button,
-  NavItem
-} from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl, NavItem } from "react-bootstrap";
 import css from "./index.css";
 import FlightSearch from "./components/Flights/FlightSearch";
 
@@ -28,7 +21,6 @@ class App extends Component {
   }
 
   setUser = user => this.setState(user);
-
   logOut = async () => {
     let res = await actions.logOut();
     this.setUser({ email: null, createdAt: null, updatedAt: null, _id: null }); //FIX
@@ -90,7 +82,11 @@ class App extends Component {
           </Navbar.Collapse>
         </Navbar>
         <Switch>
-          <Route exact path="/" render={props => <Home {...props} />} />
+          <Route
+            exact
+            path="/"
+            render={props => <Home {...props} setUser={this.setUser} />}
+          />
           <Route
             exact
             path="/sign-up"
