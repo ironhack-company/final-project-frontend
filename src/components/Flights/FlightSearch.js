b0ed4660852b911a95d9f633b2182c2c716aba68
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-11 15:41:31 -0500

    creat showFlight function to map through state, and display data

--- src/components/Flights/FlightSearch.js
+++ src/components/Flights/FlightSearch.js
@@ -1,164 +1,67 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-
-
-
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class FlightSearch extends Component {
-    constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			flights: [],
+    this.state = {
+      flights: []
+    };
+  }
 
-		}
-	}
+  componentDidMount() {
+    //get token on mount
+    fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
+      body:
+        "grant_type=client_credentials&client_id=AAAIgJEuGHf4LReD2lxXUiGEcrHHL5Q6&client_secret=PyEChDme4fGCMvzZ",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST"
+    })
+      .then(res => res.json())
+      .then(r => {
+        console.log(r);
+        let token = r.access_token; //comes here
+        const RAPIDAPI_API_URL = `https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=MAD`;
+        const RAPIDAPI_REQUEST_HEADERS = {
+          Authorization: `Bearer ${token}` //token goes here
+        };
 
+        axios
+          .get(RAPIDAPI_API_URL, { headers: RAPIDAPI_REQUEST_HEADERS }) //use token to get data
+          .then(response => {
+            const data = response.data.data;
+            console.log("data", data.data);
 
-    componentDidMount() {
-        fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
-            body: "grant_type=client_credentials&client_id=AAAIgJEuGHf4LReD2lxXUiGEcrHHL5Q6&client_secret=PyEChDme4fGCMvzZ",
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded"
-            },
-            method: "POST"
-          }).then(res => res.json()).then(r => {
-            console.log(r)
-            let token = r.access_token
-      
-            const RAPIDAPI_REQUEST_HEADERS = {
-              'Authorization': `Bearer ${token}`
-            };
-      
-            axios.get(RAPIDAPI_API_URL, { headers: RAPIDAPI_REQUEST_HEADERS })
-              .then(response => {
-                const data = response.data;
-                console.log('data', data)
-                
-                
-                this.setState({
-                    flights: response.data,
-                  
-
-                });
-
-              })
-              .catch(error => {
-                console.error('create student error', error.response)
-                alert(JSON.stringify(error.response.data))
-              })
+            this.setState({
+              flights: data //set teh flights to state
+            });
           })
-      
-          const config = {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
-          }
-      
-          axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', {
-            grant_type: 'client_credentials',
-            client_id: 'AAAIgJEuGHf4LReD2lxXUiGEcrHHL5Q6',
-            client_secret: 'PyEChDme4fGCMvzZ'
-          }, config).then(res => {
-            console.log(res, '?')
-          }).catch(err => console.log(err))
-      
-          const RAPIDAPI_API_URL = `https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=MAD`;
-      
-    }
+          .catch(error => {
+            console.error("create student error", error.response);
+          });
+      });
+  }
 
-    // showPrices = (e) => {
+  // show flights
+  showFlights = () => {
+    return this.state.flights.map((flight, index) => {
+      console.log(flight);
+      return (
+        <ul key={index}>
+          <li>From {flight.origin}</li>
+          <li>To {flight.destination}</li>
+          <li>Depart: {flight.departureDate}</li>
+          <li>Return: {flight.returnDate}</li>
+          <li>Price: ${flight.price.total}</li>
+        </ul>
+      );
+    });
+  };
 
-	// 	let copyQuotes = [...this.state.flights]
-	// 	if (copyQuotes.length > 0) {
-	// 		console.log(copyQuotes)
-	// 		return (copyQuotes.map((keyName, i) => {
-	// 			let logo = this.state.logos.filter(eachCompany => {
-	// 				return eachCompany.carrierId === keyName.InboundLeg.CarrierIds[0]
-	// 			})
-	// 			// console.log(logo[0].logo, '90909090')
-
-	// 			return (
-
-    //                 <div>
-
-    //                 </div>
-
-	// 				// <div className="flight flex">
-
-	// 				// 	<div className="flight-buy">
-	// 				// 		{logo[0] ?
-	// 				// 			<img src={logo[0].logo} className="airline-logo" alt="Turkish airlines" />
-	// 				// 			:
-	// 				// 			<img src='./images/alaska.svg' className="airline-logo" alt="Alaska airlines" />
-
-	// 				// 		}
-	// 				// 		<button>
-
-	// 				// 			{keyName.MinPrice} USD
-	// 				//   </button>
-	// 				// 	</div>
-	// 				// 	<div className="flight-info flex">
-	// 				// 		<div>
-	// 				// 			{/* <h3></h3> */}
-	// 				// 			<span>{this.state.cityTo}</span>
-	// 				// 			<span className="gray">
-	// 				// 				{/* { departure } */}
-	// 				// 				Inbound
-	// 				// 	  </span>
-	// 				// 		</div>
-	// 				// 		<div>
-	// 				// 			<span className="gray">
-	// 				// 				{/* {keyName.MinPrice} */}
-	// 				// 				🛫
-	// 				// 	  </span>
-	// 				// 		</div>
-	// 				// 		<div>
-	// 				// 			{/* <h3></h3> */}
-	// 				// 			<span>{this.state.cityFrom}</span>
-	// 				// 			<span className="gray">
-	// 				// 				Inbound
-	// 				// 	  </span>
-	// 				// 		</div>
-	// 				// 	</div>
-
-	// 				// 	<div className="flight-info2 flex2">
-	// 				// 		<div>
-	// 				// 			{/* <h3></h3> */}
-	// 				// 			<span>{this.state.cityFrom}</span>
-	// 				// 			<span className="gray">
-	// 				// 				{/* { departure } */}
-	// 				// 				Outbound
-	// 				// 	  </span>
-	// 				// 		</div>
-	// 				// 		<div>
-	// 				// 			<span className="gray">
-	// 				// 				{/* {keyName.MinPrice} USD */}
-	// 				// 				🛬
-	// 				// 	  </span>
-	// 				// 		</div>
-	// 				// 		<div>
-	// 				// 			{/* <h3></h3> */}
-	// 				// 			<span>{this.state.cityTo}</span>
-	// 				// 			<span className="gray">
-	// 				// 				Outbound
-	// 				// 	  </span>
-	// 				// 		</div>
-	// 				// 	</div>
-
-
-	// 				// </div>
-	// 			)
-	// 		}
-	// 		))
-	// 	}
-	// }
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return <div>{this.showFlights()}</div>;
+  }
 }
